3b895d73dffc677b64bc40952b400e491345f175
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2023-10-23 15:07:53 +0300

    new data on the products

--- lib/Screens/product_detail.dart
+++ lib/Screens/product_detail.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   final String title1;
-const ProductDetailScreen(this.title1, {super.key});
+  const ProductDetailScreen(this.title1, {super.key});
 
   @override
   Widget build(BuildContext context) {
+    final id = Provider.of<ProductProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(title1),
