8dcbaa20c9bf724c8f9fdf385ec43c9b6e8f70aa
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2023-10-30 17:48:27 +0300

    detail page half way done

--- lib/Screens/product_detail.dart
+++ lib/Screens/product_detail.dart
@@ -4,17 +4,50 @@ import 'package:provider/provider.dart';
 import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  // final String title1;
-  // const ProductDetailScreen(this.title1, {super.key});
   static const routeName = '/product-detail';
   @override
   Widget build(BuildContext context) {
     final productId =
         ModalRoute.of(context)?.settings.arguments as String; // gives us the id
-    final jovic = Provider.of<ProductProvider>(context,listen: false).findbyId(productId);
+    final jovic = Provider.of<ProductProvider>(context, listen: false)
+        .findbyId(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(jovic.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(jovic.imageUrl, fit: BoxFit.cover),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${jovic.price}',
+              style: const TextStyle(
+                  color: Colors.blue,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                jovic.description,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
