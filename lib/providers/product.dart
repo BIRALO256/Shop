1941c163caa4c114d31fc38b4689871dec5a29f2
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2023-11-16 17:14:47 +0300

    isFavorite working , though not prpoerly

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Products with ChangeNotifier {
   final String id;
@@ -16,9 +19,33 @@ class Products with ChangeNotifier {
     required this.imageUrl,
     this.isFavorite = false,
   });
-  // Method to toggle the favorite status
-  void toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
+
+  void _isFavValue(bool newValue) {
+    isFavorite = newValue;
     notifyListeners();
+  }
+
+  // Method to toggle the favorite status
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
+    isFavorite = !isFavorite;
+    final url = Uri.parse(
+        'https://shop-e599a-default-rtdb.firebaseio.com/products/$id.json');
+    notifyListeners();
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode(
+          {
+            'isFavorite': isFavorite,
+          },
+        ),
+      );
+      if (response.statusCode >= 400) {
+        _isFavValue(oldStatus);
+      }
+    } catch (error) {
+      _isFavValue(oldStatus);
+    }
   }
 }
